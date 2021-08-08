7655d738320b409b682f94c037de32115d3d9838
KyrieXu11 <kyriexu11@126.com>
2021-08-08 15:42:53 +0800

    add config group

--- git_manager/git_manager.go
+++ git_manager/git_manager.go
@@ -1,20 +1,47 @@
 package git_manager
 
 import (
+	"flag"
 	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
 )
 
+/*
+[https]
+       proxy = https://127.0.0.1:7890
+[http]
+       proxy = http://127.0.0.1:7890
+*/
+
+var (
+	ConfigList []*ConfigGroup
+	conf       *GMConfig
+)
+
+type GMConfig struct {
+	path string `yaml:"file-path"`
+}
+
 type Config struct {
-	Proxy *NetWorkProxy
+	Http  *HTTP
+	Https *HTTPS
 	User  *User
 }
 
-type NetWorkProxy struct {
-	HttpProxy  string
-	HttpsProxy string
+type NetWork struct {
+	Proxy string
+}
+
+type HTTP struct {
+	NetWork
+}
+
+type HTTPS struct {
+	NetWork
 }
 
 type User struct {
@@ -25,6 +52,56 @@ type User struct {
 type ConfigGroup struct {
 	GroupName string
 	Config    *Config
+}
+
+func init() {
+	bs, err := ioutil.ReadFile("etc/gm_config.yml")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if err = yaml.Unmarshal(bs, conf); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func Add(groupName string, config *Config) {
+	set(groupName, config)
+}
+
+func set(groupName string, config *Config) {
+	validation := IsGitRepo()
+	if !validation {
+		return
+	}
+	for _, group := range ConfigList {
+		if group.GroupName == groupName {
+			group.set(groupName, config)
+			return
+		}
+	}
+	group := &ConfigGroup{
+		GroupName: groupName,
+		Config:    config,
+	}
+	ConfigList = append(ConfigList, group)
+}
+
+func Sync() {
+
+}
+
+func (c *ConfigGroup) set(groupName string, config *Config) {
+	if c.GroupName != "" {
+		if c.GroupName != groupName {
+			fmt.Println("group name is not exist")
+			return
+		}
+	} else {
+		c.GroupName = groupName
+	}
+	*(c.Config) = *config
 }
 
 func GetCurrentPath() (string, error) {
@@ -63,4 +140,9 @@ func RunCmd(name string, arg ...string) (string, error) {
 	}
 	// 报错时 exit status 1
 	return string(msg), err
+}
+
+func Main(f *flag.Flag) {
+
+	defer Sync()
 }
